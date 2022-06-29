67316706b7039532fb4c488d593ea38a11ada4bc
frh-w <fwaleed001@gmail.com>
2022-06-29 18:13:12 +0300

    part 2

--- code.js
+++ code.js
@@ -1,2 +1,13 @@
 let grade = prompt("Enter your grade :");
 console.log(grade);
+if (grade >= 90) {
+  console.log("You got a A !");
+} else if (90 > grade >= 80) {
+  console.log("You got a B ");
+} else if (80 > grade >= 70) {
+  confirm.log(" You got a C ");
+} else if (70 > grde >= 60) {
+  console.log("Yoy got a D ");
+} else if (60 > grade) {
+  console.log("You failed :( ");
+}
